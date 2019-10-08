e814c25f1dee0c1c133106d1eb788ebaa49ed58b
David R <jossedaviid1@gmail.com>
2019-10-08 14:53:11 -0500

    logout request

--- platziVideo/src/components/Header.jsx
+++ platziVideo/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logoutRequest } from '../actions'
 import { connect } from 'react-redux';
 import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userProfile from '../assets/static/user-icon.png'
+import Login from '../containers/Login';
 const Header = props => {
   const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({})
+  }
   return(
     <header className="header">
       <Link to="/">
@@ -14,12 +20,15 @@ const Header = props => {
       </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
-            <img src={Object.keys(user).length > 0 ? gravatar(user.email) : userProfile} alt={user.email} />
+            <img src={hasUser ? gravatar(user.email) : userProfile} alt={user.email} />
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href="/">Cuenta</a></li>
-          <li><Link to="/login">Iniciar Sesión</Link></li>
+          {hasUser && <li><a href="/">{user.email}</a></li>}
+          {hasUser ? 
+            <li><Link to="#logout" onClick={handleLogout}>Cerrar Sesión</Link></li> : 
+            <li><Link to="/login">Iniciar Sesión</Link></li>
+          }
         </ul>
       </div>
     </header>
@@ -31,4 +40,7 @@ const mapStateToProps = state => {
     user: state.user
   }
 }
-export default connect(mapStateToProps, null)(Header)
+const mapDispatchToProps = {
+  logoutRequest,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
