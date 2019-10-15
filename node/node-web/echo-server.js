3231af04af1c100b93178c550a580e7181cb20a0
David R <jossedaviid1@gmail.com>
2019-10-15 16:32:01 -0500

    date in node

--- node/node-web/echo-server.js
+++ node/node-web/echo-server.js
@@ -5,9 +5,32 @@ server.on('request', (request, response) => {
   if(request.method === 'POST' && request.url == "/echo"){
     // response.statusCode = 200;
     // response.setHeader("Content-Type", "text/plain");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("hi w...\n");
-  }else{
+
+    let body = [];
+    request.on('data', chunk =>{
+      body.push(chunk);
+    })
+    .on('end', () => {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      body = Buffer.concat(body).toString();
+      response.end(body);
+    })
+
+  }else if(request.method === 'POST' && request.url == "/date"){
+    let body = [];
+    request.on('data', chunk =>{
+      body.push(chunk);
+    })
+    .on('end', () => {
+      response.writeHead(200, {"Content-Type": "text/plain"})
+      body = Buffer.concat(body).toString();
+      const birthDay = new Date(body);
+      const day = birthDay.getDay() + 1;
+      const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+      const exactDay = days[day];
+      response.end(`${exactDay}`);
+    })
+  } else{
     response.statusCode = 404;
     response.end();
   }
