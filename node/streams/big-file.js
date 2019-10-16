4343cf28796eb1abbe49d90b01823db57b2f7976
David R <jossedaviid1@gmail.com>
2019-10-16 10:03:06 -0500

    streams / server

---
+++ node/streams/big-file.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const file = fs.createWriteStream('./big');
+
+for (let i = 0; i <= 1e6; i++){
+  file.write('Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años ')
+}
+
+file.end();
