e68517f1b84246409f4ff1ed9400570ac80b1291
David R <jossedaviid1@gmail.com>
2019-10-16 16:19:34 -0500

    services in express

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -1,15 +1,71 @@
 const express = require('express');
-const { moviesMock } = require('../utils/mocks/movies');
+const MoviesService = require('../services/movies');
 
 function moviesApi(app){
   const router = express.Router();
   app.use('/api/movies', router);
+  const moviesService = new MoviesService();
+  
   router.get('/', async function(req,res,next){
+    const { tags } = req.query;
     try{
-      const movies = await Promise.resolve(moviesMock);
+      const movies = await moviesService.getMovies({tags});
       res.status(200).json({
         data: movies,
         message: 'movies listed'
+      })
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.get('/:movieId', async function(req,res,next){
+    const { movieId } = req.params;
+    try{
+      const movies = await moviesService.getMovie({movieId})
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrived'
+      })
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.post('/', async function(req,res,next){
+    const { body: movie } = req;
+    try{
+      const createMovieId = await moviesService.createMovie({movie})
+      res.status(201).json({
+        data: createMovieId,
+        message: 'movie created'
+      })
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.put('/:movieId', async function(req,res,next){
+    const { movieId } = req.params;
+    const { body: movie } = req;
+    try{
+      const updateMovieId = await moviesService.updateMovie({movieId, movie})
+      res.status(200).json({
+        data: updateMovieId,
+        message: 'update movie'
+      })
+    } catch(err){
+      next(err)
+    }
+  })
+
+  router.delete('/:movieId', async function(req,res,next){
+    const { movieId } = req.params;
+    try{
+      const deleteMovie = await moviesService.deleteMovie({movieId})
+      res.status(200).json({
+        data: deleteMovie,
+        message: 'deleted movie'
       })
     } catch(err){
       next(err)
