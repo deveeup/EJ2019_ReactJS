bf45b6b1873528e12bcb2665775ce471356df1dd
David R <jossedaviid1@gmail.com>
2019-10-25 11:59:17 -0500

    fixed ;

--- platziVideo/src/server/server.js
+++ platziVideo/src/server/server.js
@@ -15,7 +15,7 @@ if(ENV === 'development'){
   console.log('loading dev config');
   const webpackConfig = require('../../webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackConfig);
   const serverConfig = {
     contentBase: `http://localhost${PORT}`,
