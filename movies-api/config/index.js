ae8fb1fd6586d59349b2817ea197eb58f4ea7e06
David R <jossedaviid1@gmail.com>
2019-11-06 16:46:37 -0500

    seedUsers

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -1,10 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: 3000,
   dbUser: 'db_user_video',
   dbPassword: 'BOQ2MKbkGuRqg74V',
   dbHost: 'cluster0-lw3ow.mongodb.net',
-  dbName: 'pVideos_db'
+  dbName: 'pVideos_db',
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 }
 
 module.exports = { config };
